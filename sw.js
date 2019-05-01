7872e21ec669899b0d695916f1d2da160fa1ae6b
Shaun <shaun@thenetninja.co.uk>
2019-05-01 18:12:07 +0100

    lesson-15

--- sw.js
+++ sw.js
@@ -1,14 +1,31 @@
+const staticCacheName = 'site-static';
+const assets = [
+  '/',
+  '/index.html',
+  '/js/app.js',
+  '/js/ui.js',
+  '/js/materialize.min.js',
+  '/css/styles.css',
+  '/css/materialize.min.css'
+];
+
 // install event
 self.addEventListener('install', evt => {
-  console.log('service worker installed');
+  //console.log('service worker installed');
+  evt.waitUntil(
+    caches.open(staticCacheName).then((cache) => {
+      console.log('caching shell assets');
+      cache.addAll(assets);
+    })
+  );
 });
 
 // activate event
 self.addEventListener('activate', evt => {
-  console.log('service worker activated');
+  //console.log('service worker activated');
 });
 
 // fetch event
 self.addEventListener('fetch', evt => {
-  console.log('fetch event', evt);
+  //console.log('fetch event', evt);
 });
