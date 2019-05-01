13798bbaffbc27b99d2aaa46995854e24b0ce50a
Shaun <shaun@thenetninja.co.uk>
2019-05-01 17:26:34 +0100

    lesson-12

--- sw.js
+++ sw.js
@@ -7,3 +7,8 @@ self.addEventListener('install', evt => {
 self.addEventListener('activate', evt => {
   console.log('service worker activated');
 });
+
+// fetch event
+self.addEventListener('fetch', evt => {
+  console.log('fetch event', evt);
+});
