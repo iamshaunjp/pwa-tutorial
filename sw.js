1f02758bac87902d16ceb3cb5776ed4caa379509
Shaun <shaun@thenetninja.co.uk>
2019-05-02 09:55:27 +0100

    lesson-16a

--- sw.js
+++ sw.js
@@ -28,4 +28,9 @@ self.addEventListener('activate', evt => {
 // fetch event
 self.addEventListener('fetch', evt => {
   //console.log('fetch event', evt);
+  evt.respondWith(
+    caches.match(evt.request).then(cacheRes => {
+      return cacheRes || fetch(evt.request);
+    })
+  );
 });
