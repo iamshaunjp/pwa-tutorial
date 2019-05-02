c431325698f3d5f7cb2ff07718c72e0cb6bad107
Shaun <shaun@thenetninja.co.uk>
2019-05-02 11:52:07 +0100

    lesson-21

--- sw.js
+++ sw.js
@@ -14,6 +14,17 @@ const assets = [
   '/pages/fallback.html'
 ];
 
+// cache size limit function
+const limitCacheSize = (name, size) => {
+  caches.open(name).then(cache => {
+    cache.keys().then(keys => {
+      if(keys.length > size){
+        cache.delete(keys[0]).then(limitCacheSize(name, size));
+      }
+    });
+  });
+};
+
 // install event
 self.addEventListener('install', evt => {
   //console.log('service worker installed');
@@ -47,6 +58,8 @@ self.addEventListener('fetch', evt => {
       return cacheRes || fetch(evt.request).then(fetchRes => {
         return caches.open(dynamicCacheName).then(cache => {
           cache.put(evt.request.url, fetchRes.clone());
+          // check cached items size
+          limitCacheSize(dynamicCacheName, 15);
           return fetchRes;
         })
       });
