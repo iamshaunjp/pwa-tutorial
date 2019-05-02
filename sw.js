99da081773948c653cfcb3e027f8ad98de8a1e04
Shaun <shaun@thenetninja.co.uk>
2019-05-02 10:12:43 +0100

    lesson-16

--- sw.js
+++ sw.js
@@ -6,7 +6,10 @@ const assets = [
   '/js/ui.js',
   '/js/materialize.min.js',
   '/css/styles.css',
-  '/css/materialize.min.css'
+  '/css/materialize.min.css',
+  '/img/dish.png',
+  'https://fonts.googleapis.com/icon?family=Material+Icons',
+  'https://fonts.gstatic.com/s/materialicons/v47/flUhRq6tzZclQEJ-Vdg-IuiaDsNcIhQ8tQ.woff2'
 ];
 
 // install event
