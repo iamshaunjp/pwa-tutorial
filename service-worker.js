152913216ef65fe40ab1dc6942f25e3fef271a8f
James007wang <98909117+James007wang@users.noreply.github.com>
2022-04-06 14:03:12 -0400

    c

---
+++ service-worker.js
@@ -0,0 +1,51 @@
+var CACHE_NAME = 'my-web-app-version-v3';
+
+self.addEventListener('install', event => {
+    event.waitUntil(
+         caches.open(CACHE_NAME)
+         .then(cache => {
+            return cache.addAll([
+                './',
+                './index.php',
+                './ServiceWorkerUpdateListener.min.js'
+            ]);
+         })
+    );
+});
+
+self.addEventListener('activate', event => {
+    event.waitUntil(
+        caches.keys().then(cacheNames => {
+            return Promise.all(
+                cacheNames.map(cacheName => {
+                    if (cacheName !== CACHE_NAME) 
+                    return caches.delete(cacheName);
+                })
+            );
+        })
+    );
+});
+
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request)
+        .then(response => {
+            if (response) return response;
+
+            return fetch(event.request)
+            .then(response => {
+                if (!response || response.status !== 200 || response.type !== 'basic') return response;
+
+                var responseToCache = response.clone();
+
+                caches.open(CACHE_NAME).then(cache => { 
+                    cache.put(event.request, responseToCache) 
+                });
+            });
+        })
+    )
+});
+
+self.addEventListener('message', event => {
+    if (event.data === 'skipWaiting') return skipWaiting();
+});
