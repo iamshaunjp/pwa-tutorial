b4fde533ebf41c4fd036c38bb2ce7032307fa82e
James007wang <98909117+James007wang@users.noreply.github.com>
2022-04-06 13:55:47 -0400

    c

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const staticCacheName = 'site-static-v6';
-const dynamicCacheName = 'site-dynamic-v6';
+const staticCacheName = 'site-static-v7';
+const dynamicCacheName = 'site-dynamic-v7';
 const assets = [
   '/',
   '/index.html',
